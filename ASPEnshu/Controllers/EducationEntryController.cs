3e597328525eda78758577ee0be01a247b6db675
Too.Shigeyuki <shigeyuki.to@techno-creative.co.jp>
2024-09-13 11:37:39 +0900

    RPG対応

--- ASPEnshu/Controllers/EducationEntryController.cs
+++ ASPEnshu/Controllers/EducationEntryController.cs
@@ -4,26 +4,21 @@ using ASPEnshu.Data;
 using ASPEnshu.Models.ViewModels;
 using ASPEnshu.Models.Interfaces;
 using ASPEnshu.Models.Services;
+using System.Text.Json.Serialization;
+using System.Text.Json;
 
-namespace ASPEnshu.Controllers
-{
-    public class EducationEntryController : Controller
-    {
+namespace ASPEnshu.Controllers {
+    public class EducationEntryController : Controller {
         private readonly ASPEnshuContext _context;
 
         private IService<EducationVM> _service;
+        private EducationVM EducationVM { get; set; }
 
-        [TempData]
-        public int Id { get; set; }
-
-        public EducationVM EducationVM { get; set; }
-
-        public EducationEntryController(ASPEnshuContext context)
-        {
+        public EducationEntryController(ASPEnshuContext context) {
             _context = context;
             _service = new EducationService(_context);
         }
-         
+
         //初期表示（post後の再表示と兼ねる）
         [HttpGet]
         public async Task<IActionResult> EducationEntrySheet(int id) {
@@ -33,15 +28,16 @@ namespace ASPEnshu.Controllers
             }
 
             EducationVM = await _service.SetViewModel();
-            this.Id = id;
-            ViewBag.Session=HttpContext.Session.Id;      //ここはセッション管理のテストなので、演習とは関係ない
+ 
+            ViewBag.Session = HttpContext.Session.Id;      //ここはセッション管理のテストなので、演習とは関係ない
+            ViewBag.Id = id;
             return View(EducationVM);
         }
 
         //post後
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> EducationEntrySheet(EducationVM educationVM) {
+        public async Task<IActionResult> EducationEntrySheet(int Id,EducationVM educationVM) {
 
             EducationVM = await _service.AfterPostExecution(educationVM);
 
@@ -50,7 +46,23 @@ namespace ASPEnshu.Controllers
                 ViewBag.Session = HttpContext.Session.Id;    //ここはセッション管理のテストなので、演習とは関係ない
                 return View(EducationVM);
             }
-            return RedirectToAction();  //こちらがPRG(Post-Redirect-Get)対応したもの
+
+            TempData["EducationVM"] = JsonSerializer.Serialize(EducationVM);
+            ViewBag.Id = Id;
+            return RedirectToAction("Result");  //こちらがPRG(Post-Redirect-Get)対応したもの
         }
+
+        [HttpGet]
+        public async Task<IActionResult> Result(int id) {
+            if (TempData["EducationVM"] != null) {
+                EducationVM = JsonSerializer.Deserialize<EducationVM>(TempData["EducationVM"].ToString());
+                ViewBag.Session = HttpContext.Session.Id;    //ここはセッション管理のテストなので、演習とは関係ない
+                return View("EducationEntrySheet", EducationVM);
+            }
+            else {
+                return RedirectToAction("EducationEntrySheet");
+            }
+        }
+
     }
 }
